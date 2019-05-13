29b535beec09b0ff1335c66b3b9930be04bbd81b
gilad1324 <34319280+gilad1324@users.noreply.github.com>
2019-05-13 20:41:06 +0300

    Add files via upload

--- רעיונות למיניפ.sql
+++ רעיונות למיניפ.sql
@@ -1,8 +1,8 @@
-select WORKERID
+select WORKERID, WORKERWAGE
 from OEHRLICH.WORKERS
 where WORKERTASK =  'food seller'
 
-select EQUIPMENTNAME
+select distinct PROVIDERID, EQUIPMENTNAME
 from OEHRLICH.PROVIDER NATURAL JOIN OEHRLICH.EQUIPMENTFORPROVIDER NATURAL JOIN OEHRLICH.EQUIPMENT 
 WHERE PROVIDERPAYMENT < 300
 	
@@ -11,8 +11,8 @@ from OEHRLICH.FOOD
 group by FOODNAME
 
 SELECT foodname, COUNT(providerid) 
-FROM oehrlich.food natural join oehrlich.foodforprovider natural join oehrlich.provider
-WHERE foodprice < 30
+FROM oehrlich.food F1 natural join oehrlich.foodforprovider F2 natural join oehrlich.provider F3
+WHERE F1.FOODID = F2.RECEPTIONID and foodprice < 30
 GROUP BY foodname
 
 select P.providerid, P.providerpayment
@@ -21,14 +21,16 @@ where P.providerpayment = (select max(P2.providerpayment)
                           from OEHRLICH.PROVIDER P2)
 
 						  
-select costumerid from OEHRLICH.costumer
+select costumerid, costumeremail
+from OEHRLICH.costumer
 where costumeremail like '%.il'
 
-select ticketid
+select ticketid, moviename, releaseDate
 from oehrlich.tickets natural join oehrlich.ticketfortheatre natural join oehrlich.theater
+     natural join oehrlich.Ticketformovies natural join oehrlich.movies
 where ticketstatus = 'Active' and vip = 'YES'
 
-select ticketid
+select ticketid, moviename, releaseDate
 from oehrlich.tickets natural join oehrlich.ticketfortheatre natural join oehrlich.theater
+     natural join oehrlich.Ticketformovies natural join oehrlich.movies
 where ticketChair > 50 and  ticketChair < 100 and ticketLine = 15 and numofchairs < 100
-
